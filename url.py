2f686c85b95699c935e891a6ec568bf49062c8c1
aditya-prakash-singh <126975871+aditya-prakash-singh@users.noreply.github.com>
2023-11-18 20:59:49 +0530

    Update url.py

--- url.py
+++ url.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pyshorteners as ps
+import qrcode
+from PIL import Image
 print('working')
+
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants. ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("img.png")
+
 
 def shortit(url):
     try:
@@ -13,10 +29,15 @@ def shortit(url):
 
 st.header('URL Shortener')
 title = st.text_input('Enter the url', '',placeholder='Url Here')
-rdb=shortit(title)
+
 
 if st.button('Shorten Url'):
     rdb=shortit(title)
-st.write('Your Shortened URL : ',rdb)
+    generate_qrcode(title)
+    st.write('Your Shortened URL : ',rdb,'\n')
+    st.image('img.png', caption='QR',width=140)
 
 
+
+st.write('\n')
+st.write('\n')
